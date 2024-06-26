8f100bf3158e8c1a84367a6b7b5ff65ee8aed516
George Mwangi <mwangigeorge648@gmail.com>
2024-06-26 19:39:00 +0300

    Data Import

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,31 +1,38 @@
 import pandas as pd
+import plotly.express as px
 from shiny.express import ui, render, input
 from pathlib import Path
 from shinywidgets import render_plotly
-import plotly.express as px
+from data_import import df
 
-
-file_path = Path(__file__).parent / "penguins.csv"
-
-df = pd.read_csv(file_path)
 
 with ui.sidebar(bg="#4898a8"):
 
     ui.h1("Hello World")
+    ui.input_slider("mass", "Body Mass", 2500, 6500, 3000)
+    ui.input_select("sex", "Select Sex", choices=["Male", "Female"])
 
 
-ui.input_slider("mass", "Body Mass", 2500, 6500, 3000)
-ui.input_select("sex", "Select Sex", choices=["Male", "Female", None])
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Plot")
 
+        @ render_plotly
+        def plot():
+            df_sub = df[(df["sex"] == input.sex()) & (
+                df["body_mass_g"] >= input.mass())]
 
-@ render_plotly
-def plot():
-    df_sub = df[(df["sex"] == input.sex()) & (
-        df["body_mass_g"] >= input.mass())]
-    return px.scatter(df_sub, "bill_depth_mm", "bill_length_mm", color="species", facet_col="island")
+            if input.show_species():
+                return px.scatter(df_sub, "bill_depth_mm", "bill_length_mm", color="species")
+            else:
+                return px.scatter(df_sub, "bill_depth_mm", "bill_length_mm")
 
+        ui.input_checkbox("show_species", "Show Species", value=True)
 
-@ render.data_frame
-def dat():
-    return df[(df["sex"] == input.sex()) & (
-        df["body_mass_g"] >= input.mass())]
+    with ui.card():
+        ui.card_header("Raw Data")
+
+        @ render.data_frame
+        def dat():
+            return df[(df["sex"] == input.sex()) & (
+                df["body_mass_g"] >= input.mass())]
