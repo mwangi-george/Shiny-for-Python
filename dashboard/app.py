9a74d85fe63ece95a94581a417c621bd199052a2
George Mwangi <mwangigeorge648@gmail.com>
2024-06-26 16:02:46 +0300

    Initial Commit

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,12 +9,14 @@ df = pd.read_csv(file_path)
 
 ui.h1("Hello World")
 
-ui.input_slider("mass", "Body Mass", 200, 3000, 400)
+ui.input_slider("mass", "Body Mass", 2000, 3000, 400)
 
 
 @render.data_frame
 def dat():
-    return df
+    val = input.mass()
+    print(val)
+    return df[df["body_mass_g"] < val]
 
 
 @render.text
