74aaf690ccb9b1b09836eac84885fe5606392bb6
George Mwangi <mwangigeorge648@gmail.com>
2024-06-26 15:58:09 +0300

    Initial Commit

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,15 @@
-from shiny.express import ui, render
+from shiny.express import ui, render, input
 from pathlib import Path
 import pandas as pd
 
 
-file_path = Path(__file__).parent // "penguins.csv"
+file_path = Path(__file__).parent / "penguins.csv"
 
 df = pd.read_csv(file_path)
 
 ui.h1("Hello World")
+
+ui.input_slider("mass", "Body Mass", 200, 3000, 400)
 
 
 @render.data_frame
